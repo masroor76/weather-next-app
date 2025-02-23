898b477d506b0ba9b7cd37dcf766634968b2fe2a
Masroor Amir <63614513+masroor76@users.noreply.github.com>
2025-02-24 04:03:28 +0500

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/Components/Footer";
+import NavBar from "@/Components/NavBar"
+
+export default function Home() {
+
+  return (
+    <div>
+      <NavBar />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1>Hello from TypeScript Weather Next App</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/app/weather/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            I am from weather page!.
+        </div>
+    )
+}
+
+export default page
